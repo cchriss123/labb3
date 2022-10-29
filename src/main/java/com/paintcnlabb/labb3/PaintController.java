b1cea4c2b2135c724309e09a0b993228e9788cc8
cchriss123 <christofer.nystrom@iths.se>
2022-10-29 17:04:12 +0200

    added updateMethod

--- src/main/java/com/paintcnlabb/labb3/PaintController.java
+++ src/main/java/com/paintcnlabb/labb3/PaintController.java
@@ -1,15 +1,15 @@
 package com.paintcnlabb.labb3;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 
 public class PaintController {
+
+    paintModel paintModel = new paintModel();
 
     public Canvas canvas;
     public GraphicsContext context;
@@ -20,22 +20,32 @@ public class PaintController {
         context = canvas.getGraphicsContext2D();
     }
 
-    public void onCanvasDragged(MouseEvent mouseEvent) {
-
+    public void onCanvasClicked(MouseEvent mouseEvent) {
         double size = Double.parseDouble(brushSize.getText());
+        //paintModel.addShape(colorPicker.getValue(), size, mouseEvent.getX(), mouseEvent.getY());
+        paintModel.shapes.add(new Shape(colorPicker.getValue(), size, mouseEvent.getX(), mouseEvent.getY()));
+        update();
+    }
 
-        context.setFill(colorPicker.getValue());
-        //context.fillRect(mouseEvent.getX()-(size/2), mouseEvent.getY()-(size/2),size,size);
-        context.fillRoundRect(mouseEvent.getX()-(size/2), mouseEvent.getY()- (size/2), size,size,size,size);
-
-
-
+    private void update() {
+        context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        for (var shape : paintModel.shapes) {
+            shape.draw(context);
+        }
     }
 
     public void save(ActionEvent actionEvent) {
+        //TODO
     }
 
     public void exit(ActionEvent actionEvent) {
         System.exit(0);
     }
+
+    public void onUndoAction(ActionEvent event) {
+        paintModel.undoShape();
+        update();
+    }
+
 }
+
---
+++ src/main/java/com/paintcnlabb/labb3/Shape.java
@@ -0,0 +1,55 @@
+package com.paintcnlabb.labb3;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Shape {
+    private Color color;
+    private double size;
+    private double x;
+    private double y;
+
+    public Shape(Color color, double size, double x, double y) {
+        this.color = color;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void draw(GraphicsContext context){
+        context.setFill(this.getColor());
+        context.fillOval(getX()-(size/2),getY()- (size/2), size,size);
+
+    }
+}
