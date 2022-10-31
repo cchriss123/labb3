5fa12a619a4ae4291ad4ff8cb356717c4e8e2926
cchriss123 <christofer.nystrom@iths.se>
2022-10-31 16:30:54 +0100

    added color property

--- src/main/java/com/paintcnlabb/labb3/paintModel.java
+++ src/main/java/com/paintcnlabb/labb3/paintModel.java
@@ -1,5 +1,6 @@
 package com.paintcnlabb.labb3;
 
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.paint.Color;
@@ -10,22 +11,28 @@ import java.nio.file.Path;
 
 public class paintModel {
 
+    ObjectProperty<Color> color;
     ObservableList<Shape> shapes = FXCollections.observableArrayList();
 
 
-/*
-    void addCircle(Color currentColor, double size, double x, double y) {
-        shapes.add(new Circle(currentColor, size, x, y));
+    public paintModel() {
+        this.color = new SimpleObjectProperty<>(Color.BLACK);
     }
+
+    void addCircle(Color color, double size, double x, double y) {
+        shapes.add(new Circle(color, size, x, y));
+    }
+
     void addSquare(Color currentColor, double size, double x, double y) {
         shapes.add(new Square(currentColor, size, x, y));
     }
+
 
     public void undoShape() {
         shapes.remove(shapes.size()-1);
     }
 
- */
+
 
     public void saveToFile(Path file) {
         StringBuffer outPut = new StringBuffer();
@@ -44,6 +51,14 @@ public class paintModel {
         catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Property<Color> getColorProperty() {
+        return color;
+    }
+
+    public Color getColor() {
+        return color.get();
     }
 }
 
