de20e196132c96b5b485557df00c2092cda3824e
cchriss123 <christofer.nystrom@iths.se>
2022-11-05 02:20:10 +0100

    added save to svg

---
+++ src/main/java/com/paintcnlabb/labb3/SvgWriter.java
@@ -0,0 +1,41 @@
+package com.paintcnlabb.labb3;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SvgWriter {
+
+    FileChooser fileChooser = new FileChooser();
+    List<String> svgStrings = new ArrayList<>();
+
+    public void save(PaintModel paintModel, Stage stage) {
+
+        fileChooser.setTitle("Save as");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.getExtensionFilters().clear();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("SVG File", "*.svg"));
+        File filePath = fileChooser.showSaveDialog(stage);
+
+        buildSvgString(paintModel);
+
+        try {
+            assert filePath != null;
+            Files.write(filePath.toPath(), svgStrings);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void buildSvgString(PaintModel paintModel) {
+        svgStrings.add("<svg width=\"800.0\" height=\"600.0\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">");
+        paintModel.shapes.forEach(shape -> svgStrings.add(shape.writeSVG()));
+        svgStrings.add("</svg>");
+    }
+}
