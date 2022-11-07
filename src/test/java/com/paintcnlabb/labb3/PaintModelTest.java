4281c7b931eb52a025e01a507258751b636fafca
cchriss123 <christofer.nystrom@iths.se>
2022-11-08 00:17:34 +0100

    bug fixed

--- src/test/java/com/paintcnlabb/labb3/PaintModelTest.java
+++ src/test/java/com/paintcnlabb/labb3/PaintModelTest.java
@@ -1,16 +1,6 @@
 package com.paintcnlabb.labb3;
-
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.paint.Color;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 
 import static com.paintcnlabb.labb3.ShapeType.TRIANGLE;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,10 +12,9 @@ class PaintModelTest {
     @Test
     void createShapeCreatesCorrectShapeType() {
         paintModel.createShape(TRIANGLE, 100, 100);
-        var expected = TRIANGLE;
-        var actual = paintModel.shapes.size();
+        var actual = paintModel.shapes.get(0).getShapeType();
 
-        assertEquals(expected, TRIANGLE);
+        assertEquals(actual, TRIANGLE);
     }
 
     @Test
